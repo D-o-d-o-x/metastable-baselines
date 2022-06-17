b3b9bf1a9d471e8dd5429e2174f879ee93e66373
Dominik Roth <dominik.roth.dev@gmail.com>
2022-06-17 11:29:06 +0200

    Moved py-files to into subfolder

---
+++ sb3_trl/trl_pg/__init__.py
@@ -0,0 +1,2 @@
+from sb3_trl.trl_pg.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from sb3_trl.trl_pg.trl_pg import TRL_PG
---
+++ sb3_trl/trl_sac/__init__.py
@@ -0,0 +1,2 @@
+from sb3_trl.trl_sac.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from sb3_trl.trl_sac.trl_sac import TRL_SAC
