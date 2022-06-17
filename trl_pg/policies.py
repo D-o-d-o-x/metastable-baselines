c90805fdff4290d4ee96a546b2a8dccb910feb43
Dominik Roth <dominik.roth.dev@gmail.com>
2022-06-17 11:31:25 +0200

    Removed old files (moved into sb3_trl/)

--- trl_pg/policies.py
+++
@@ -1,7 +0,0 @@
-# This file is here just to define MlpPolicy/CnnPolicy
-# that work for TRL_PG
-from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, MultiInputActorCriticPolicy
-
-MlpPolicy = ActorCriticPolicy
-CnnPolicy = ActorCriticCnnPolicy
-MultiInputPolicy = MultiInputActorCriticPolicy
--- trl_sac/__init__.py
+++
@@ -1,2 +0,0 @@
-from sb3_trl.trl_sac.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-from sb3_trl.trl_sac.trl_sac import TRL_SAC
